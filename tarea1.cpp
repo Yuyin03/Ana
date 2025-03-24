df129034f28471d796bbc8be8b7e425262ff4a72
Yuyin03 <rosaliovianney@gmail.com>
2025-03-24 11:07:52 -0600

    Add files via upload

---
+++ tarea1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    double resultado;
+    double x= 2.0 - (1.0 / 4.0);
+    double y= 1.0 + (1.0 / x);
+    double z = 2.0 / y;
+    resultado = 5.0 + (2.0 / z);
+    
+    printf("%.2f\n", resultado);
+
+    return 0;
+}
