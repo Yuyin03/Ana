644be706ab9c413ca847d08f9ada26cac17a730f
Yuyin03 <rosaliovianney@gmail.com>
2025-03-24 11:02:08 -0600

    Add files via upload

---
+++ 20250324-105619.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+   double a =90;
+   double b= 70;
+   double c = 478;
+    printf("%f", b*c*a);
+    printf ("\n");
+    return 0;
+}
---
+++ 20250324-105831.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+   double minu =60;
+   double hora= 60;
+   double dia = 24;
+   double anio= 365;
+   double edad=18;
+    printf("%f", edad*anio*dia*hora*minu);
+    printf ("\n");
+    return 0;
+}
